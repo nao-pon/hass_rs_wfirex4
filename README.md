d46e83949177d6443666c8c2a2d05630a75fd33f
nao-pon <hypweb@gmail.com>
2021-08-13 20:56:22 +0900

    Added a config option `mac`

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![hacs_badge](https://img.shields.io/badge/HACS-Custom-orange.svg?style=for-the-badge)](https://github.com/custom-components/hacs)
+
 # Sensor fetcher and IR contoroler for RS-WFIREX4
 
 A custom componet of Home Assistant to use RS-WFIREX4. This component is still under development. Do not use it in a production environment.
@@ -21,13 +23,15 @@ In other words, the configuration directory of homeassistant is where the config
 
 ```
 rs_wfirex4:
-  - host: "xxx.xxx.xxx.xxx" # IP address of your first RS-WFIREX4
-    name: "Living Wfirex4"  # Optional entity name
-    scan_interval: 30       # Optional seconds of scan interval (Default 60)
+  - host: "xxx.xxx.xxx.xxx"   # IP address of your first RS-WFIREX4
+    mac : "xx:xx:xx:xx:xx:xx" # Optional MAC Address of this device (Recommend to set for device identification)
+    name: "Living Wfirex4"    # Optional entity name
+    scan_interval: 30         # Optional seconds of scan interval (Default 60)
 
-  - host: "xxx.xxx.xxx.xxx" # IP address of your second RS-WFIREX4
-    name: "Living Wfirex4"  # Optional entity name
-    scan_interval: 30       # Optional seconds of scan interval (Default 60)
+  - host: "xxx.xxx.xxx.xxx"   # IP address of your second RS-WFIREX4
+    mac : "xx:xx:xx:xx:xx:xx" # Optional MAC Address of this device (Recommend to set for device identification)
+    name: "Living Wfirex4"    # Optional entity name
+    scan_interval: 30         # Optional seconds of scan interval (Default 60)
 ```
 You can find entities that four sensors and one remote.
 
--- custom_components/rs_wfirex4/__init__.py
+++ custom_components/rs_wfirex4/__init__.py
@@ -2,11 +2,13 @@
 
 from getmac import get_mac_address
 
+import logging
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 
 from homeassistant.helpers import discovery
-from homeassistant.const import CONF_HOST, CONF_NAME, CONF_SCAN_INTERVAL
+from homeassistant.helpers.device_registry import format_mac
+from homeassistant.const import CONF_HOST, CONF_NAME, CONF_SCAN_INTERVAL, CONF_MAC
 
 DOMAIN = 'rs_wfirex4'
 DEFAULT_NAME = 'RS-WFIREX4'
@@ -15,10 +17,13 @@ PLATFORMS = ['remote', 'sensor']
 CONF_TEMP_OFFSET = 'temp_offset'
 CONF_HUMI_OFFSET = 'humi_offset'
 
+_LOGGER = logging.getLogger(__name__)
+
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema([
         vol.Schema({
             vol.Required(CONF_HOST): cv.string,
+            vol.Optional(CONF_MAC, default=''): cv.string,
             vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
             vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): cv.positive_int,
             vol.Optional(CONF_TEMP_OFFSET, default=0.0): vol.All(vol.Coerce(float), vol.Range(min=-10, max=10)),
@@ -29,7 +34,18 @@ CONFIG_SCHEMA = vol.Schema({
 
 async def async_setup(hass, configs):
     for config in configs.get(DOMAIN):
-        config['uid'] = get_mac_address(ip=config.get(CONF_HOST)).replace(':', '')
+        if (config.get(CONF_MAC) == ''):
+            ip = config.get(CONF_HOST)
+            mac = get_mac_address(ip)
+            _LOGGER.info("Detected Mac address as %s.", mac)
+            if mac is None or mac == "00:00:00:00:00:00":
+                ips = ip.split('.')
+                mac = "00:1C:C2:" + format(int(ips[1]), '02X') + ":" + format(int(ips[2]), '02X') + ":" + format(int(ips[3]), '02X')
+                _LOGGER.warning("The MAC address could not be detected automatically. Use \"%s\" as the fake address. You can specify the correct MAC address with the \"mac\" option for more perfect operation.", mac)
+
+            config['uid'] = format_mac(mac)
+        else:
+            config['uid'] = format_mac(config.get(CONF_MAC))
         for component in PLATFORMS:
             await hass.helpers.discovery.async_load_platform(component, DOMAIN, config, configs)
 
